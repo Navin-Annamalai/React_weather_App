2ebbab9c58e546e8cc5efc45e0fa5c4b2c9bc2b2
Navin-Annamalai <navin1696.a@gmail.com>
2025-04-10 00:04:14 +0530

    1st Commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Weather from "./Weather";
+
+let api_key = "912d826163770e0f2444a785334db68e";
+
+function App() {
+  // const [temp, setTemp] = useState(0);
+  // const [city, setCity] = useState("Chennai");
+  // const [country, setcountry] = useState("India");
+  // const [long, setLong] = useState(0);
+  const [text, setText] = useState("Chennai");
+  // const [wind, setWind] = useState(0)
+const [weatherObj, setWeatherObj ] = useState({temp:0,city:"Chennai", country:"India", wind:0, long:0});
+
+  const search = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api_key}&units=Metric`;
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+      // setTemp(Math.floor(data.main.temp));
+      // setCity(data.name);
+      // setcountry(data.sys.country)
+      // setWind(data.wind.speed);
+      const obj = {temp:Math.floor(data.main.temp),city:data.name, country:data.sys.country, wind:data.wind.speed, long:0}
+      setWeatherObj(obj)
+
+    } catch (error) {
+      alert("Please Check your Spelling/City not Found");
+    } 
+  };
+  const handlecity = (e) => {
+    setText(e.target.value);
+  };
+
+  const handlekeydown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+  return (
+    <>
+
+<div className="m-auto flex-wrap justify-center w-full max-w-md p-10 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 h-screen flex items-center bg-gradient-to-r from-gray-100 to-blue-100">
+<h1 className="font-extrabold text-blue-500 text-4xl"> Weather App</h1>
+ <div >
+        <input
+          type="text"
+          className=" border-2 rounded-md px-3 py-1.5"
+          placeholder="Search city"
+          onChange={handlecity}
+          value={text}
+          onKeyDown={handlekeydown}
+        />
+        <button className="px-5 py-2 bg-blue-500 text-white font-bold rounded-md ml-8" onClick={search}>Go</button>
+      </div>
+<div className=" mr-15 text-2xl mb-20">
+<Weather
+obj = {weatherObj}
+      />
+</div>
+ </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Weather.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+function Weather(props) {
+  console.log(props.obj);
+  
+  const {temp, city, country, wind} = props.obj;
+  
+  return (
+    <>
+<div className="image"></div>
+      <div className="temp p-1">Today's Temp - <span className="text-blue-500 font-extrabold p-1">{temp}c</span> </div>
+      <div className="city p-1"> City - <span className="text-blue-500 font-extrabold">{city}</span></div>
+      <div className="country p-1">Country - <span className="text-blue-500 font-extrabold">{country}</span></div>
+      <div className="windspeed p-1">Wind Speed - <span className="text-blue-500 font-extrabold">{wind}</span> Km/hr</div>
+      <div className="cord">
+      </div>
+
+    </>
+  );
+}
+
+export default Weather;
